3098f06155e82e508e0e6de2a7f8ce55aea3aa18
vincent_g <vincent.grossier@infostrates.fr>
2016-09-16 14:42:11 +0200

    Initial Commit

---
+++ FeedingHistoryLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeedingHistoryLab
+//
+//  Created by vincent on 16/09/2016.
+//  Copyright © 2016 infostrates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
